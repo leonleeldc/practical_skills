380c671a6947130dec7ae20d59d2a9a15a59834c
Dingcheng Li <dingchengl@gmail.com>
2023-03-09 18:16:21 -0800

    update

--- scala_tStoreQueryEntities.scala
+++ scala_tStoreQueryEntities.scala
@@ -2,7 +2,7 @@ package com.coupang.search.pk
 
 import org.apache.log4j.{LogManager, Logger}
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, Row, SaveMode, SparkSession}
 import org.sellmerfud.optparse._
 import com.coupang.search.pk.utils.S3Utils
 import org.apache.spark.sql.expressions.UserDefinedFunction
@@ -39,133 +39,81 @@ object CalculateQueryStoreEntityCoverage {
     getGenericEntityNames _
   )
 
-  // def udfGetStoreQueryEntitiesStats: UserDefinedFunction = udf(
-  //   getStoreQueryEntitiesStats _
-  // )
+   def udfGetStoreQueryEntitiesStats: UserDefinedFunction = udf(
+     getStoreQueryEntitiesStats _
+   )
 
-  // def getStoreQueryEntitiesStats(storeQueryEntity: Seq[Row]): StoreQueryEntityResult = {
-  //   if (storeQueryEntity == null || storeQueryEntity.isEmpty) {
-  //     return StoreQueryEntityResult()
-  //   }
+  case class StoreQueryEntityResult(
+                                     storeQueryModelEntityExposes2w: Option[Int] = None,
+                                     storeQueryModelEntityPurchases2w: Option[Int] = None,
+                                     storeQueryBrandEntityExposes3m: Option[Int] = None,
+                                     storeQueryBrandEntityPurchases3m: Option[Int] = None,
+                                     storeQueryBrandEntityClicks3m: Option[Int] = None,
+                                     storeQueryFlavorEntityClicks2w: Option[Int] = None,
+                                     storeQueryPackageTypeEntityClicks2w: Option[Int] = None,
+                                     storeQueryTypeEntityExposes2w: Option[Int] = None,
+                                     storeQueryTypeEntityPurchases2w: Option[Int] = None,
+                                     storeQueryCustomerEntityQueryFeature3m: Option[Int] = None,
+                                     solrCustomerClickedEntityPreferenceRealtime: Option[Int] = None,
+                                     solrCustomerQueryEntityPreference3m: Option[Int] = None,
+                                     solrCustomerPurchasedEntityPreference: Option[Int] = None,
+                                     solrCustomerClickedEntityPreference: Option[Int] = None
+                                   )
 
-  //     val storeQueryModelEntityExposes2w: Option[Integer] = None
-  //     val storeQueryModelEntityPurchases2w: Option[Integer] = None
-  //     val storeQueryBrandEntityExposes3m: Option[Integer] = None
-  //     val storeQueryBrandEntityPurchases3m: Option[Integer] = None
-  //     val storeQueryBrandEntityClicks3m: Option[Integer] = None
-  //     val storeQueryFlavorEntityClicks2w: Option[Integer] = None
-  //     val storeQueryPackageTypeEntityClicks2w: Option[Integer] = None
-  //     val storeQueryTypeEntityExposes2w: Option[Integer] = None
-  //     val storeQueryTypeEntityPurchases2w: Option[Integer] = None
-  //     val storeQueryCustomerEntityQueryFeature3m: Option[Integer] = None
-  //     val solrCustomerClickedEntityPreferenceRealtime: Option[Integer] = None
-  //     val solrCustomerQueryEntityPreference3m: Option[Integer] = None
-  //     val solrCustomerPurchasedEntityPreference: Option[Integer] = None
-  //     val solrCustomerClickedEntityPreference: Option[Integer] = None
+  def getStoreQueryEntitiesStats(storeQueryEntity: Seq[Row]): StoreQueryEntityResult = {
+    if (storeQueryEntity == null || storeQueryEntity.isEmpty) {
+      return StoreQueryEntityResult()
+    }
 
-  //   storeQueryEntity.foreach((row: Row) => {
-  //       storeQueryModelEntityExposes2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(0))
-  //       } else {
-  //         None
-  //       }
+    var storeQueryModelEntityExposes2w: Option[Int] = None
+    var storeQueryModelEntityPurchases2w: Option[Int] = None
+    var storeQueryBrandEntityExposes3m: Option[Int] = None
+    var storeQueryBrandEntityPurchases3m: Option[Int] = None
+    var storeQueryBrandEntityClicks3m: Option[Int] = None
+    var storeQueryFlavorEntityClicks2w: Option[Int] = None
+    var storeQueryPackageTypeEntityClicks2w: Option[Int] = None
+    var storeQueryTypeEntityExposes2w: Option[Int] = None
+    var storeQueryTypeEntityPurchases2w: Option[Int] = None
+    var storeQueryCustomerEntityQueryFeature3m: Option[Int] = None
+    var solrCustomerClickedEntityPreferenceRealtime: Option[Int] = None
+    var solrCustomerQueryEntityPreference3m: Option[Int] = None
+    var solrCustomerPurchasedEntityPreference: Option[Int] = None
+    var solrCustomerClickedEntityPreference: Option[Int] = None
 
-  //       storeQueryModelEntityPurchases2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(1))
-  //       } else {
-  //         None
-  //       }
+    storeQueryEntity.foreach((row: Row) => {
+      storeQueryModelEntityExposes2w = Option(row.getInt(0))
+      storeQueryModelEntityPurchases2w = Option(row.getInt(1))
+      storeQueryBrandEntityExposes3m = Option(row.getInt(2))
+      storeQueryBrandEntityPurchases3m = Option(row.getInt(3))
+      storeQueryBrandEntityClicks3m = Option(row.getInt(4))
+      storeQueryFlavorEntityClicks2w = Option(row.getInt(5))
+      storeQueryPackageTypeEntityClicks2w = Option(row.getInt(6))
+      storeQueryTypeEntityExposes2w = Option(row.getInt(7))
+      storeQueryTypeEntityPurchases2w = Option(row.getInt(8))
+      storeQueryCustomerEntityQueryFeature3m = Option(row.getInt(9))
+      solrCustomerClickedEntityPreferenceRealtime = Option(row.getInt(10))
+      solrCustomerQueryEntityPreference3m = Option(row.getInt(11))
+      solrCustomerPurchasedEntityPreference = Option(row.getInt(12))
+      solrCustomerClickedEntityPreference = Option(row.getInt(13))
+    }
 
-  //       storeQueryBrandEntityExposes3m = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(2))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryBrandEntityPurchases3m = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(3))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryBrandEntityClicks3m = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(4))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryFlavorEntityClicks2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(5))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryPackageTypeEntityClicks2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(6))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryTypeEntityExposes2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(7))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryTypeEntityPurchases2w = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(8))
-  //       } else {
-  //         null
-  //       }
-
-  //       storeQueryCustomerEntityQueryFeature3m = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(9))
-  //       } else {
-  //         null
-  //       }
-
-  //       solrCustomerClickedEntityPreferenceRealtime = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(10))
-  //       } else {
-  //         null
-  //       }
-
-  //       solrCustomerQueryEntityPreference3m = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(11))
-  //       } else {
-  //         null
-  //       }
-
-  //       solrCustomerPurchasedEntityPreference = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(12))
-  //       } else {
-  //         null
-  //       }
-
-  //       solrCustomerClickedEntityPreference = if (!row.isNullAt(0)) {
-  //         Some(row.getInt(13))
-  //       } else {
-  //         null
-  //       }
-        
-  //   })
-  //   StoreQueryEntityResult(
-  //     storeQueryModelEntityExposes2w,
-  //     storeQueryModelEntityPurchases2w,
-  //     storeQueryBrandEntityExposes3m,
-  //     storeQueryBrandEntityClicks3m,
-  //     storeQueryBrandEntityClicks3m,
-  //     storeQueryFlavorEntityClicks2w,
-  //     storeQueryPackageTypeEntityClicks2w,
-  //     storeQueryTypeEntityExposes2w,
-  //     storeQueryTypeEntityPurchases2w,
-  //     storeQueryCustomerEntityQueryFeature3mr,
-  //     solrCustomerClickedEntityPreferenceReanteger,
-  //     solrCustomerQueryEntityPreference3m,
-  //     solrCustomerPurchasedEntityPreference,
-  //     solrCustomerClickedEntityPreference
-  //     )
-  // }
+     StoreQueryEntityResult(
+       storeQueryModelEntityExposes2w,
+       storeQueryModelEntityPurchases2w,
+       storeQueryBrandEntityExposes3m,
+       storeQueryBrandEntityClicks3m,
+       storeQueryBrandEntityClicks3m,
+       storeQueryFlavorEntityClicks2w,
+       storeQueryPackageTypeEntityClicks2w,
+       storeQueryTypeEntityExposes2w,
+       storeQueryTypeEntityPurchases2w,
+       storeQueryCustomerEntityQueryFeature3mr,
+       solrCustomerClickedEntityPreferenceReanteger,
+       solrCustomerQueryEntityPreference3m,
+       solrCustomerPurchasedEntityPreference,
+       solrCustomerClickedEntityPreference
+       )
+   }
 
   def getEntityResult(dfQeuryEntity: DataFrame): DataFrame = {
     val dfEntityResult = dfQeuryEntity
