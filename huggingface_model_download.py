831000373d06c69f3b88a0c819c0ecb19b80a0eb
Dingcheng Li <dingchengl@gmail.com>
2023-03-19 10:12:54 -0700

    add model saving and loading

--- huggingface_model_download.py
+++ huggingface_model_download.py
@@ -3,3 +3,13 @@ from transformers import AutoModel, AutoTokenizer
 model_name = "snunlp/KR-BERT-char16424"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModel.from_pretrained(model_name)
+# Choose a directory to save the model and tokenizer
+save_directory = "path/to/your/save_directory"
+# Save the tokenizer
+tokenizer.save_pretrained(save_directory)
+# Save the model
+model.save_pretrained(save_directory)
+
+# Load the tokenizer and model from the save directory
+loaded_tokenizer = AutoTokenizer.from_pretrained(save_directory)
+loaded_model = AutoModel.from_pretrained(save_directory)
